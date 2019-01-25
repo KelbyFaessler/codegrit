5c540e69514238c3dcdc657ee63fc40298d0b3ad
Kelby Faessler <kmfaessler@gmail.com>
2019-01-24 18:53:56 -0800

    added several new pages

--- codegrit/core/urls.py
+++ codegrit/core/urls.py
@@ -5,4 +5,7 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('about/', views.about, name='about'),
+    path('contact/', views.contact, name='contact'),
+    path('signup/', views.signup, name='signup'),
+    path('terms/', views.terms, name='terms'),
 ]
--- codegrit/core/views.py
+++ codegrit/core/views.py
@@ -13,7 +13,8 @@ def index(request):
 
 def about(request):
     """
-    about page
+    about page: has copy and redirects to the signup form page via
+                the signup button at the end of the copy
     """
 
     return render(
@@ -21,3 +22,36 @@ def about(request):
         'about.html',
         context={}
     )
+
+def contact(request):
+    """
+    contact page: simple page that just provides support email address
+    """
+
+    return render(
+        request,
+        'contact.html',
+        context={}
+    )
+
+def signup(request):
+    """
+    signup page: form to collect user info if they want to sign up
+    """
+
+    return render(
+        request,
+        'signup.html',
+        context={}
+    )
+
+def terms(request):
+    """
+    terms and conditions page: basic terms, gives credibility to site
+    """
+
+    return render(
+        request,
+        'terms.html',
+        context={}
+    )    
